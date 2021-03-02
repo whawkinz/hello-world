cd0cfede28ac8a10b2d741df7c3b802c8ea3e2f6
whawkinz <70980645+whawkinz@users.noreply.github.com>
2021-03-01 20:50:00 -0700

    Update GitDemo.java
    Added name to file, implemented addNumber() function, added "reverseNum" variable to "GitDemo" class and assigned it newNumber in reverseNumber() function.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Wade Hawkins
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,6 +15,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
+        previousNumber = previousNumber + num;
         return 0;
     }
     
@@ -45,7 +47,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
